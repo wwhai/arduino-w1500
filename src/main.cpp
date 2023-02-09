7dd8744177fdeb3866d5dbd04d9f3967ae2f000e
wwhai <cnwwhai@gmail.com>
2023-02-09 21:00:44 +0800

    dev: dhcp

--- src/main.cpp
+++ src/main.cpp
@@ -1,81 +1,69 @@
 #include <SPI.h>
 #include <Ethernet.h>
-#include <Dns.h>
-#define HOST_NAME "Arduino001"
 
-byte mac[] = {0xA8, 0xA1, 0x59, 0x2E, 0xA2, 0xD8};
-
-EthernetServer server(80);
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 
 void setup()
 {
+
   Serial.begin(9600);
-  delay(100);
-  Serial.println("Ethernet start...");
-  Ethernet.begin(mac, IPAddress(192, 168, 1, 111));
-  Ethernet.setGatewayIP(IPAddress(192, 168, 1, 1));
-  Ethernet.setSubnetMask(IPAddress(255, 255, 255, 0));
-  Ethernet.setDnsServerIP(IPAddress(192, 168, 1, 1));
-  server.begin();
+  while (!Serial)
+    ;
+
+  Serial.println("Initialize Ethernet with DHCP:");
+  if (Ethernet.begin(mac) == 0)
+  {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    if (Ethernet.hardwareStatus() == EthernetNoHardware)
+    {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    }
+    else if (Ethernet.linkStatus() == LinkOFF)
+    {
+      Serial.println("Ethernet cable is not connected.");
+    }
+    while (true)
+    {
+      delay(1);
+    }
+  }
+  // print your local IP address:
+  Serial.print("My IP address: ");
+  Serial.println(Ethernet.localIP());
 }
 
 void loop()
 {
-  // listen for incoming clients
-  EthernetClient client = server.available();
-  if (client)
+  switch (Ethernet.maintain())
   {
-    Serial.println("new client");
-    // an HTTP request ends with a blank line
-    bool currentLineIsBlank = true;
-    while (client.connected())
-    {
-      if (client.available())
-      {
-        char c = client.read();
-        Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the HTTP request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank)
-        {
-          // send a standard HTTP response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close"); // the connection will be closed after completion of the response
-          client.println("Refresh: 5");        // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          // output the value of each analog input pin
-          for (int analogChannel = 0; analogChannel < 6; analogChannel++)
-          {
-            int sensorReading = analogRead(analogChannel);
-            client.print("analog input ");
-            client.print(analogChannel);
-            client.print(" is ");
-            client.print(sensorReading);
-            client.println("<br />");
-          }
-          client.println("</html>");
-          break;
-        }
-        if (c == '\n')
-        {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        }
-        else if (c != '\r')
-        {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    // give the web browser time to receive the data
-    delay(1);
-    // close the connection:
-    client.stop();
-    Serial.println("client disconnected");
+  case 1:
+    // renewed fail
+    Serial.println("Error: renewed fail");
+    break;
+
+  case 2:
+    // renewed success
+    Serial.println("Renewed success");
+    // print your local IP address:
+    Serial.print("My IP address: ");
+    Serial.println(Ethernet.localIP());
+    break;
+
+  case 3:
+    // rebind fail
+    Serial.println("Error: rebind fail");
+    break;
+
+  case 4:
+    // rebind success
+    Serial.println("Rebind success");
+    // print your local IP address:
+    Serial.print("My IP address: ");
+    Serial.println(Ethernet.localIP());
+    break;
+
+  default:
+    // nothing happened
+    break;
   }
 }
